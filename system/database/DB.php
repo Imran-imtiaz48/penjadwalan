4d90802f98d7c6776c3487f770c920bfb88de7bf
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-07-02 07:43:02 +0400

    Update DB.php
    Key Differences:
    •	Uses exceptions instead of show_error for critical errors.
    •	Uses class_alias() instead of eval().
    •	Adds strict types, improved input validation, and modern PHP features.
    •	Enhanced commenting and docblocks.
    •	Follows modern best practices for maintainability and security.

--- system/database/DB.php
+++ system/database/DB.php
@@ -1,162 +1,105 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP 5.1.6 or newer
- *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
- * @filesource
- */
+<?php
+declare(strict_types=1);
 
-// ------------------------------------------------------------------------
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Initialize the database
+ * Initialize the database connection
  *
- * @category	Database
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/database/
- * @param 	string
- * @param 	bool	Determines if active record should be used or not
+ * @param  string|array $params Database connection group or DSN string or config array
+ * @param  bool|null    $active_record_override Whether to use active record
+ * @return mixed        Database connection object
+ * @throws Exception    If configuration or DSN is invalid
  */
-function &DB($params = '', $active_record_override = NULL)
+function &DB($params = '', $active_record_override = null)
 {
-	// Load the DB config file if a DSN string wasn't passed
-	if (is_string($params) AND strpos($params, '://') === FALSE)
-	{
-		// Is the config file in the environment folder?
-		if ( ! defined('ENVIRONMENT') OR ! file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/database.php'))
-		{
-			if ( ! file_exists($file_path = APPPATH.'config/database.php'))
-			{
-				show_error('The configuration file database.php does not exist.');
-			}
-		}
+    // Load config if not DSN string
+    if (is_string($params) && strpos($params, '://') === false) {
+        $envPath = defined('ENVIRONMENT') ? APPPATH . 'config/' . ENVIRONMENT . '/database.php' : '';
+        $defaultPath = APPPATH . 'config/database.php';
 
-		include($file_path);
+        if (!empty($envPath) && file_exists($envPath)) {
+            include($envPath);
+        } elseif (file_exists($defaultPath)) {
+            include($defaultPath);
+        } else {
+            throw new Exception('The configuration file database.php does not exist.');
+        }
 
-		if ( ! isset($db) OR count($db) == 0)
-		{
-			show_error('No database connection settings were found in the database config file.');
-		}
+        if (!isset($db) || !is_array($db) || empty($db)) {
+            throw new Exception('No database connection settings were found in the database config file.');
+        }
 
-		if ($params != '')
-		{
-			$active_group = $params;
-		}
+        $active_group = $params ?: ($active_group ?? null);
+        if (empty($active_group) || !isset($db[$active_group])) {
+            throw new Exception('You have specified an invalid database connection group.');
+        }
 
-		if ( ! isset($active_group) OR ! isset($db[$active_group]))
-		{
-			show_error('You have specified an invalid database connection group.');
-		}
+        $params = $db[$active_group];
+    } elseif (is_string($params)) {
+        // Parse DSN
+        $dns = @parse_url($params);
+        if ($dns === false) {
+            throw new Exception('Invalid DB Connection String');
+        }
 
-		$params = $db[$active_group];
-	}
-	elseif (is_string($params))
-	{
+        $params = [
+            'dbdriver' => $dns['scheme'] ?? '',
+            'hostname' => isset($dns['host']) ? rawurldecode($dns['host']) : '',
+            'username' => isset($dns['user']) ? rawurldecode($dns['user']) : '',
+            'password' => isset($dns['pass']) ? rawurldecode($dns['pass']) : '',
+            'database' => isset($dns['path']) ? rawurldecode(ltrim($dns['path'], '/')) : '',
+        ];
 
-		/* parse the URL from the DSN string
-		 *  Database settings can be passed as discreet
-		 *  parameters or as a data source name in the first
-		 *  parameter. DSNs must have this prototype:
-		 *  $dsn = 'driver://username:password@hostname/database';
-		 */
+        // Additional config items
+        if (isset($dns['query'])) {
+            parse_str($dns['query'], $extra);
+            foreach ($extra as $key => $val) {
+                if (strcasecmp($val, 'TRUE') === 0) {
+                    $val = true;
+                } elseif (strcasecmp($val, 'FALSE') === 0) {
+                    $val = false;
+                }
+                $params[$key] = $val;
+            }
+        }
+    }
 
-		if (($dns = @parse_url($params)) === FALSE)
-		{
-			show_error('Invalid DB Connection String');
-		}
+    if (empty($params['dbdriver'])) {
+        throw new Exception('You have not selected a database type to connect to.');
+    }
 
-		$params = array(
-							'dbdriver'	=> $dns['scheme'],
-							'hostname'	=> (isset($dns['host'])) ? rawurldecode($dns['host']) : '',
-							'username'	=> (isset($dns['user'])) ? rawurldecode($dns['user']) : '',
-							'password'	=> (isset($dns['pass'])) ? rawurldecode($dns['pass']) : '',
-							'database'	=> (isset($dns['path'])) ? rawurldecode(substr($dns['path'], 1)) : ''
-						);
+    // Load DB classes
+    require_once(BASEPATH . 'database/DB_driver.php');
+    $useActiveRecord = $active_record_override ?? true;
 
-		// were additional config items set?
-		if (isset($dns['query']))
-		{
-			parse_str($dns['query'], $extra);
+    if ($useActiveRecord) {
+        require_once(BASEPATH . 'database/DB_active_rec.php');
+        if (!class_exists('CI_DB', false)) {
+            class_alias('CI_DB_active_record', 'CI_DB');
+        }
+    } else {
+        if (!class_exists('CI_DB', false)) {
+            class_alias('CI_DB_driver', 'CI_DB');
+        }
+    }
 
-			foreach ($extra as $key => $val)
-			{
-				// booleans please
-				if (strtoupper($val) == "TRUE")
-				{
-					$val = TRUE;
-				}
-				elseif (strtoupper($val) == "FALSE")
-				{
-					$val = FALSE;
-				}
+    require_once(BASEPATH . 'database/drivers/' . $params['dbdriver'] . '/' . $params['dbdriver'] . '_driver.php');
+    $driverClass = 'CI_DB_' . $params['dbdriver'] . '_driver';
 
-				$params[$key] = $val;
-			}
-		}
-	}
+    if (!class_exists($driverClass)) {
+        throw new Exception("Database driver class {$driverClass} not found.");
+    }
 
-	// No DB specified yet?  Beat them senseless...
-	if ( ! isset($params['dbdriver']) OR $params['dbdriver'] == '')
-	{
-		show_error('You have not selected a database type to connect to.');
-	}
+    $DB = new $driverClass($params);
 
-	// Load the DB classes.  Note: Since the active record class is optional
-	// we need to dynamically create a class that extends proper parent class
-	// based on whether we're using the active record class or not.
-	// Kudos to Paul for discovering this clever use of eval()
+    if (!empty($DB->autoinit)) {
+        $DB->initialize();
+    }
 
-	if ($active_record_override !== NULL)
-	{
-		$active_record = $active_record_override;
-	}
+    if (!empty($params['stricton'])) {
+        $DB->query('SET SESSION sql_mode="STRICT_ALL_TABLES"');
+    }
 
-	require_once(BASEPATH.'database/DB_driver.php');
-
-	if ( ! isset($active_record) OR $active_record == TRUE)
-	{
-		require_once(BASEPATH.'database/DB_active_rec.php');
-
-		if ( ! class_exists('CI_DB'))
-		{
-			eval('class CI_DB extends CI_DB_active_record { }');
-		}
-	}
-	else
-	{
-		if ( ! class_exists('CI_DB'))
-		{
-			eval('class CI_DB extends CI_DB_driver { }');
-		}
-	}
-
-	require_once(BASEPATH.'database/drivers/'.$params['dbdriver'].'/'.$params['dbdriver'].'_driver.php');
-
-	// Instantiate the DB adapter
-	$driver = 'CI_DB_'.$params['dbdriver'].'_driver';
-	$DB = new $driver($params);
-
-	if ($DB->autoinit == TRUE)
-	{
-		$DB->initialize();
-	}
-
-	if (isset($params['stricton']) && $params['stricton'] == TRUE)
-	{
-		$DB->query('SET SESSION sql_mode="STRICT_ALL_TABLES"');
-	}
-
-	return $DB;
+    return $DB;
 }
-
-
-
-/* End of file DB.php */
-/* Location: ./system/database/DB.php */
